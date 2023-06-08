b3cc901843296ba5cb23f26153b5fb8797df699b
Mihndim <mihndim2016@gmail.com>
2023-06-08 09:46:41 +0100

    Add comments component

---
+++ comments/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser"); // you don't need to install an extra package for this
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 4001;
+
+const commentsByPostId = {};
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+  res.status(201).send(comments);
+});
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.status(200).send(commentsByPostId[req.params.id] || []);
+});
+
+app.listen(port, () => {
+  console.log(`The server is listening on port: ${port}`);
+});

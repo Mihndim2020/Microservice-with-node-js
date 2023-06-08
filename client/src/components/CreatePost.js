eeb84d95cb85996f06863a4bea665bda5a246afa
Mihndim <mihndim2016@gmail.com>
2023-06-08 12:07:03 +0100

    Add cors

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+const url = "http://localhost:4000/posts";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post(url, {
+      title,
+    });
+    console.log(response);
+    setTitle("");
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Title</label>
-          <input className="form-control" />
+          <input
+            value={title}
+            onChange={handleChange}
+            className="form-control"
+          />
         </div>
       </form>
-      <button className="btn btn-primary">Submit</button>
+      <button onClick={handleSubmit} className="btn btn-primary">
+        Submit
+      </button>
     </div>
   );
 };

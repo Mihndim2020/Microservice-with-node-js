245be095ec85afb6819f02472932da498bda1077
Mihndim <mihndim2016@gmail.com>
2023-06-15 10:09:31 +0100

    Add PostList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
 
 function App() {
   return (
     <div className="App">
       <h1>Create Post</h1>
       <CreatePost />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 }
